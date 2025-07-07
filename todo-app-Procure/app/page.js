bbd8aa6c68a07cc882753d44a9e68884122ad7c5
ParagNukte <paragnuktek@gmail.com>
2025-07-08 01:04:04 +0530

    added a landing page

--- todo-app-Procure/app/page.js
+++ todo-app-Procure/app/page.js
@@ -1,8 +1,15 @@
+import Link from "next/link";
 import "./globals.css";
 export default function Home() {
   return (
-    <div>
-      This is the home page of the Todo App. You can manage your todos here.
+    <div className="flex flex-col items-center justify-center min-h-screen bg-primary-bg p-4 sm:p-6 lg:p-8 animate-fadeIn text-black gap-8">
+      <div className="flex text-2xl ">Get to work with your Todo App!</div>
+      <Link href="/todos">
+        {" "}
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors duration-200">
+          Click Me
+        </button>
+      </Link>
     </div>
   );
 }
